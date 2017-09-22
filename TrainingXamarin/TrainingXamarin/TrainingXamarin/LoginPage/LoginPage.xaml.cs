2fe1063f0e61f42eb9c24c885ad2800f72448271
nguyen.ngoc.sonc <nguyen.ngoc.sonc@framgias-Mac-mini.local>
2017-09-22 08:11:37 +0700

    Finish login page

---
+++ TrainingXamarin/TrainingXamarin/TrainingXamarin/LoginPage/LoginPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace TrainingXamarin
+{
+    public partial class LoginPage : ContentPage
+    {
+        int i = 0;
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        async void login_Clicked(object sender, EventArgs e)
+        {
+            var userName = username.Text;
+            var passWord = password.Text;
+
+            if (userName == "admin" && passWord == "admin")
+            {
+                if (i == 0)
+                {
+                    await Navigation.PushAsync(new IntroPage());
+                }
+                else
+                {
+                    await Navigation.PushAsync(new MainPage());
+                }
+                i++;
+            }
+            else
+            {
+                await DisplayAlert("Alert", "Wrong username or password!", "OK");
+            }
+        }
+    }
+}
