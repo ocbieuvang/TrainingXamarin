c7b4ddb539e266251c5c1d356a0a226ab20736fc
datchiz <mai.quoc.dat@vn-macbook-0068s-Mac-mini.local>
2017-10-12 16:06:07 +0700

    auto complete text and set location for android

---
+++ TrainingXamarin/TrainingXamarin/Droid/AutoCompleteViewRenderer.cs
@@ -0,0 +1,44 @@
+﻿using TrainingXamarin.Control;
+using Xamarin.Forms;
+using TrainingXamarin.Droid;
+using Android.Widget;
+using System.ComponentModel;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(AutoCompleteView), typeof(AutoCompleteViewRenderer))]
+namespace TrainingXamarin.Droid
+{
+    public class AutoCompleteViewRenderer : ViewRenderer<AutoCompleteView, AutoCompleteTextView>
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<AutoCompleteView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement != null || this.Element == null)
+                return;
+
+            var autoComplete = new AutoCompleteTextView(Forms.Context);
+            SetNativeControl(autoComplete);
+            Control.TextChanged += OnTextChanged;
+            Control.ItemClick += OnItemClick;
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+            if (Element == null || Control == null)
+                return;
+            Control.Adapter = new ArrayAdapter(Forms.Context, Android.Resource.Layout.SimpleDropDownItem1Line, Element.ItemsSource);
+        }
+
+        async void OnTextChanged(object sender, Android.Text.TextChangedEventArgs e)
+        {
+            if (Control.Text != null) Element.TextChanged = Control.Text;
+        }
+
+        async void OnItemClick(object sender, AdapterView.ItemClickEventArgs e)
+        {
+            if (Control.Text != null) Element.ItemChanged = Control.Text;
+        }
+    }
+}
---
+++ TrainingXamarin/TrainingXamarin/TrainingXamarin/CustomView/CustomMap.cs
@@ -0,0 +1,24 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace TrainingXamarin.CustomView
+{
+    public class CustomMap : Map
+    {
+        public static readonly BindableProperty PositionChangedProperty =
+            BindableProperty.Create<CustomMap, Position>(p => p.PositionChanged, new Position(), BindingMode.TwoWay, propertyChanged: PositionPropertyChanged);
+
+        public Position PositionChanged
+        {
+            get { return (Position)GetValue(PositionChangedProperty); }
+            set { SetValue(PositionChangedProperty, value); }
+        }
+
+        private static void PositionPropertyChanged(BindableObject bindable, Position oldValue, Position newValue)
+        {
+            var control = (CustomMap)bindable;
+            if (control == null) return;
+            control.MoveToRegion(MapSpan.FromCenterAndRadius(newValue, Distance.FromMiles(1)));
+        }
+    }
+}
